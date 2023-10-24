aa5536e8c37ee4873ffd3f0de037582b18c44b63
Jamirul Islam <jamirulislam344@gmail.com>
2023-10-24 23:01:46 +0530

    .to-do-app Bg Changed

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-const addText =document.getElementById("add-text");
-const list= document.getElementById("list-container");
+const addText = document.getElementById("add-text");
+const list = document.getElementById("list-container");
 
-function addTask(){
-    if(addText.value ===''){
+function addTask() {
+    if (addText.value === '') {
         alert("Please Enter Task");
     }
-    else{
-        let li= document.createElement("li");
+    else {
+        let li = document.createElement("li");
         li.innerHTML = addText.value;
         list.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML= "&#10005;";
+        span.innerHTML = "&#10005;";
         li.appendChild(span);
     }
-    addText.value="";
+    addText.value = "";
     saveData();
 
 }
 
-list.addEventListener("click" , function(e){
-    if(e.target.tagName === "LI"){
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
     }
-    else if(e.target.tagName === "SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false);
+}, false);
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", list.innerHTML);
 }
 
-function showData(){
+function showData() {
     list.innerHTML = localStorage.getItem("data")
 }
 
